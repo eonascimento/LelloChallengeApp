2026a24360d6a6900c7029d52a67c5720e5f6657
Ênio Oliveira <eonascimento@stefainini.com>
2021-01-25 22:10:58 -0300

    Filtrar lista por Login

--- lello-challenge-app/src/app/membros/lista/lista.component.ts
+++ lello-challenge-app/src/app/membros/lista/lista.component.ts
@@ -1,30 +1,39 @@
 import { MembroService } from './../services/membro.service';
 import { Membro } from './../models/membro.model';
 import { Component, OnInit } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
 })
 export class ListaComponent implements OnInit {
-
   public membros: Membro[];
+  public membrosFiltrado: Membro[];
+  filtro: string = "";
   errorMessage: string;
   constructor(private membroService: MembroService ) { }
 
   ngOnInit(): void {
-    // this.membros = new Array<Membro>();
-    // var membro = new Membro()
-    // membro.login = 'CaerusKaru';
-    // membro.avatar_url = 'https://avatars.githubusercontent.com/u/416563?v=4';
-    // this.membros.push(membro);
     this.carregarMembros()
+  }
+
+  filtrar() {
+    this.membrosFiltrado = this.membros.filter( m => m.login.toLowerCase().includes(this.filtro.trim().toLowerCase()));
+  }
+
+  limparFIltro(){
+    this.membrosFiltrado = this.membros;
+    this.filtro = '';
   }
 
   carregarMembros(){
     this.membroService.obterTodos()
         .subscribe(
-          result => this.membros = result,
+          result => {
+            this.membros = result,
+            this.membrosFiltrado = result
+          },
           error => this.errorMessage);
   }
 
